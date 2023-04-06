43d8f4d5f81be3d30966e134e6500c22ed1c8edc
Barbarisko <helen.fedchuk@gmail.com>
2023-04-06 20:16:42 +0300

    enhance ui for phone

--- vue-webapp/ItemTracker/src/pages/Login.vue
+++ vue-webapp/ItemTracker/src/pages/Login.vue
@@ -21,12 +21,12 @@ export default {
         },
         async onGo() {
             if (this.username == "" || this.password == "")
-            //give some error
+                //give some error
                 return;
 
             async function sha256(message) {
                 // encode as UTF-8
-                const msgBuffer = new TextEncoder().encode(message);                    
+                const msgBuffer = new TextEncoder().encode(message);
 
                 // hash the message
                 const hashBuffer = await crypto.subtle.digest('SHA-256', msgBuffer);
@@ -49,7 +49,7 @@ export default {
                 window.location.hash = "#/home"
             }
             else {
-                            //give some error
+                //give some error
 
                 return;
             }
@@ -62,25 +62,26 @@ export default {
     <div class="row align-items-center mt-5">
         <div class="container">
             <div class="row justify-content-center">
-                <div class="col-sm-10 col-md-8 col-lg-6 btn-group" role="group"
-                    aria-label="Basic radio toggle button group">
-                    <input type="radio" class="btn-check" name="btnradio" id="modeLogin" autocomplete="off"
-                        @change="onChangeMode" checked>
-                    <label class="btn btn-outline-primary" for="modeLogin">
-                        <h4>Login</h4>
-                    </label>
+                <div class="col-sm-12 col-md-8 col-lg-6">
+                    <div class="btn-group ps-4 pe-4" style="width: 100%;" role="group" aria-label="Basic radio toggle button group">
+                        <input type="radio" class="btn-check" name="btnradio" id="modeLogin" autocomplete="off"
+                            @change="onChangeMode" checked>
+                        <label class="btn btn-outline-primary fs-4" for="modeLogin">
+                            Login
+                        </label>
 
-                    <input type="radio" class="btn-check" name="btnradio" id="modeRegister" autocomplete="off"
-                        @change="onChangeMode">
-                    <label class="btn btn-outline-primary" for="modeRegister">
-                        <h4>Register</h4>
-                    </label>
+                        <input type="radio" class="btn-check" name="btnradio" id="modeRegister" autocomplete="off"
+                            @change="onChangeMode">
+                        <label class="btn btn-outline-primary fs-4" for="modeRegister">
+                            Register
+                        </label>
+                    </div>
                 </div>
             </div>
-            <div class="row justify-content-center">
-                <div class="col-sm-10 col-md-8 col-lg-6 pt-4">
+            <div class="row justify-content-center mt-4">
+                <div class="col-sm-10 col-md-8 col-lg-6">
 
-                    <div class="input-group mb-3">
+                    <div class="input-group ps-4 pe-4">
                         <span class="input-group-text">@</span>
                         <div class="form-floating">
                             <input type="text" class="form-control" id="floatingInputGroup1" placeholder="Username"
@@ -89,7 +90,7 @@ export default {
                         </div>
                     </div>
 
-                    <div class="input-group mb-3">
+                    <div class="input-group ps-4 pe-4 mt-2">
                         <span class="input-group-text" style="width: 42px;">*</span>
                         <div class="form-floating">
                             <input type="text" class="form-control" id="floatingInputGroup1" placeholder="Password"
@@ -98,7 +99,7 @@ export default {
                         </div>
                     </div>
 
-                    <div v-if="registerMode" class="input-group mb-3">
+                    <div v-if="registerMode" class="input-group ps-4 pe-4 mt-2">
                         <span class="input-group-text" style="width: 42px;">*</span>
                         <div class="form-floating">
                             <input type="text" class="form-control" id="floatingInputGroup1" placeholder="Password">
@@ -108,7 +109,7 @@ export default {
 
                 </div>
             </div>
-            <div class="row justify-content-center">
+            <div class="row justify-content-center mt-4">
                 <div class="col-4 mt-4 position-relative">
                     <button type="button" class="btn btn-primary position-absolute start-50 translate-middle fs-4"
                         style="width: 200px;" @click="onGo">
